3f9888555d1f7f86c616c829dc480acdd001c4db
Yufang Liu <yufang.liu@mail.utoronto.ca>
2018-03-25 19:27:39 -0400

    typo

--- vanilla_gan.py
+++ vanilla_gan.py
@@ -221,7 +221,7 @@ def training_loop(train_dataloader, opts):
                 save_samples(G, fixed_noise, iteration, opts)
             
             #delete this line later!!!
-            if G_loss.data < min_G_loss:
+            if G_loss.data[0] < min_G_loss:
                 save_samples_low_loss(G, fixed_noise, iteration, opts)
                 print('Iteration [{:4d}/{:4d}] | D_real_loss: {:6.4f} | D_fake_loss: {:6.4f} | G_loss: {:6.4f}'.format(
                        iteration, total_train_iters, D_real_loss.data[0], D_fake_loss.data[0], G_loss.data[0]))
